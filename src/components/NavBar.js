7c1edc3ba8b020a5f71e0674a21e84518690a1ed
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-08-04 23:29:13 +0300

    Completed assighnment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,30 @@
+// import React from "react";
+
+// function NavBar() {
+//   const links = ["home", "about", "projects"];
+
+//   let href = links.map((url)=>{
+//     return <a key={url} href={`#${url}`}>{url}</a>
+//   })
+
+//   return <nav>{href}</nav>;
+// }
+
+// export default NavBar;
 import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
